622505e404296e4baa096dcb8145083d03231d5c
AymaneBouhandi <aymanebhndi@gmail.com>
2023-06-17 20:28:40 -0700

    ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Deacription : print a quote using write func
+ *	size_t write(int fd, const void buffer, size_t count)
+ *
+ * Return : 1 (error)
+ *
+ */
+
+int main(void)
+{
+	char quot[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
+
+		write(1,quot, 59);
+
+		return (1):
+}
