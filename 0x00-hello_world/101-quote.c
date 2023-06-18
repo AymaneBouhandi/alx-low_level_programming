95e3f7b9f2a47f7864d96712337a16e00fbb92ef
AymaneBouhandi <aymanebhndi@gmail.com>
2023-06-17 20:38:36 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,14 +7,11 @@
  *	size_t write(int fd, const void buffer, size_t count)
  *
  * Return: 1 (error)
- *
- */
+*/
 
 int main(void)
-{
-	char quot[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
+{	char quot[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
 
-		write(1, quot, 59);
-
-		return (1);
+	write(1, quot, 59);
+	return (1);
 }
