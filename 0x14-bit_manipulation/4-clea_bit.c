5e425c6740cac5d4774858c7cc2cb6eb271f50bf
AymaneBouhandi <aymanebhndi@gmail.com>
2023-11-23 17:38:46 -0800

    ok

--- 0x14-bit_manipulation/4-clea_bit.c
+++ 0x14-bit_manipulation/4-clea_bit.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * clear_bit - function that sets the value of a bit to 0 at a given index
- * @n: input unsigned long number
- * @index: index starting from "0" of the bit we want to clear
- * Return: success "1", otherwise "-1".
-*/
-
+ * clear_bit - This prog sets the value of a bit to 0 at a given index
+ * @n: parameter
+ * @index: index
+ * Return: 1 if success, -1 if error
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
+	if (index > sizeof(n) * 8)
 		return (-1);
-
-	if (*n & 1L << index)
-		*n ^= 1L << index;
+	*n &= ~(1 << index);
 	return (1);
 }
--- 0x14-bit_manipulation/5-flip_bit.c
+++ 0x14-bit_manipulation/5-flip_bit.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * clear_bit - function that sets the value of a bit to 0 at a given index
- * @n: input unsigned long number
- * @index: index starting from "0" of the bit we want to clear
- * Return: success "1", otherwise "-1".
-*/
-
-int clear_bit(unsigned long int *n, unsigned int index)
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (index >= sizeof(n) * 8)
-		return (-1);
+	int a, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	if (*n & 1L << index)
-		*n ^= 1L << index;
-	return (1);
+	for (a = 63; a >= 0; a--)
+	{
+		current = exclusive >> a;
+		if (current & 1)
+			countbit++;
+	}
+
+	return (countbit);
 }
